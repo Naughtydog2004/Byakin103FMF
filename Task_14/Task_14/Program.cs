f4f63ab9209134620f5840bde25a64fd6e5c4d9e
Naughtydog2004 <78705979+Naughtydog2004@users.noreply.github.com>
2023-06-08 07:07:23 +0300

    Add files via upload

---
+++ Task_14/Task_14/Program.cs
@@ -0,0 +1,539 @@
+﻿using System;
+using System.Threading;
+namespace Task_14
+{
+    class MyInput
+    {
+        public static void Exit()
+        {
+            while (true)
+            {
+                ConsoleKeyInfo e;
+                Console.WriteLine();
+                Console.WriteLine("Программа завершена");
+                Console.Write("Начать заново? (Enter - да / Esc - выход): ");
+                e = Console.ReadKey();
+
+                if (e.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine("\n");
+                    //Console.Clear();
+                    Program.Main();
+                }
+                else if (e.Key == ConsoleKey.Escape)
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    //Console.Clear();
+                    continue;
+                }
+            }
+        }
+        public static int Int32(string text, string type, bool check)
+        {
+            int vari = 0;
+            ConsoleKeyInfo e;
+            //Console.WriteLine();
+            do
+            {
+                if (!check)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Некорректные данные. Нажмите Enter");
+                }
+                try
+                {
+                    if (check == false)
+                    {
+                        while (true)
+                        {
+                            e = Console.ReadKey(true);
+                            if (e.Key == ConsoleKey.Escape)
+                            {
+                                MyInput.Exit();
+                            }
+                            else if (e.Key == ConsoleKey.Enter)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    Console.Write(text);
+                    vari = Convert.ToInt32(Console.ReadLine());
+                    check = true;
+                }
+                catch
+                {
+                    check = false;
+                }
+                if (check)
+                {
+                    switch (type)
+                    {
+                        case "-0+":
+                            break;
+                        case "-+":
+                            if (vari == 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                        case "0+":
+                            if (vari < 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                        case "+":
+                            if (vari <= 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                        case "-0":
+                            if (vari > 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                        case "-":
+                            if (vari >= 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                    }
+                }
+            }
+            while (!check);
+            return vari;
+        }
+
+        public static uint UInt32(string text, string type, bool check)
+        {
+            uint vari = 0;
+            ConsoleKeyInfo e;
+            //Console.WriteLine();
+            do
+            {
+                if (!check)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Некорректные данные. Нажмите Enter");
+                }
+                try
+                {
+                    if (check == false)
+                    {
+                        while (true)
+                        {
+                            e = Console.ReadKey(true);
+                            if (e.Key == ConsoleKey.Escape)
+                            {
+                                MyInput.Exit();
+                            }
+                            else if (e.Key == ConsoleKey.Enter)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    Console.Write(text);
+                    vari = Convert.ToUInt32(Console.ReadLine());
+                    check = true;
+                }
+                catch
+                {
+                    check = false;
+                }
+                if (check)
+                {
+                    switch (type)
+                    {
+                        case "0+":
+                            if (vari < 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                        case "+":
+                            if (vari <= 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                    }
+                }
+            }
+            while (!check);
+            return vari;
+        }
+        public static long Int64(string text, string type, bool check)
+        {
+            long vari = 0;
+            ConsoleKeyInfo e;
+            //Console.WriteLine();
+            do
+            {
+                if (!check)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Некорректные данные. Нажмите Enter");
+                }
+                try
+                {
+                    if (check == false)
+                    {
+                        while (true)
+                        {
+                            e = Console.ReadKey(true);
+                            if (e.Key == ConsoleKey.Escape)
+                            {
+                                MyInput.Exit();
+                            }
+                            else if (e.Key == ConsoleKey.Enter)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    Console.Write(text);
+                    vari = Convert.ToInt64(Console.ReadLine());
+                    check = true;
+                }
+                catch
+                {
+                    check = false;
+                }
+                if (check)
+                {
+                    switch (type)
+                    {
+                        case "-0+":
+                            break;
+                        case "-+":
+                            if (vari == 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                        case "0+":
+                            if (vari < 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                        case "+":
+                            if (vari <= 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                        case "-0":
+                            if (vari > 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                        case "-":
+                            if (vari >= 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                    }
+                }
+            }
+            while (!check);
+            return vari;
+        }
+
+        public static ulong UInt64(string text, string type, bool check)
+        {
+            ulong vari = 0;
+            ConsoleKeyInfo e;
+            //Console.WriteLine();
+            do
+            {
+                if (!check)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Некорректные данные. Нажмите Enter");
+                }
+                try
+                {
+                    if (check == false)
+                    {
+                        while (true)
+                        {
+                            e = Console.ReadKey(true);
+                            if (e.Key == ConsoleKey.Escape)
+                            {
+                                MyInput.Exit();
+                            }
+                            else if (e.Key == ConsoleKey.Enter)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    Console.Write(text);
+                    vari = Convert.ToUInt64(Console.ReadLine());
+                    check = true;
+                }
+                catch
+                {
+                    check = false;
+                }
+                if (check)
+                {
+                    switch (type)
+                    {
+                        case "0+":
+                            if (vari < 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                        case "+":
+                            if (vari <= 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                    }
+                }
+            }
+            while (!check);
+            return vari;
+        }
+        public static double Double(string text, string type, bool check)
+        {
+            double vari = 0;
+            ConsoleKeyInfo e;
+            //Console.WriteLine();
+            do
+            {
+                if (!check)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Некорректные данные. Нажмите Enter");
+                }
+                try
+                {
+                    if (check == false)
+                    {
+                        while (true)
+                        {
+                            e = Console.ReadKey(true);
+                            if (e.Key == ConsoleKey.Escape)
+                            {
+                                MyInput.Exit();
+                            }
+                            else if (e.Key == ConsoleKey.Enter)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    Console.Write(text);
+                    vari = Convert.ToDouble(Console.ReadLine());
+                    check = true;
+                }
+                catch
+                {
+                    check = false;
+                }
+                if (check)
+                {
+                    switch (type)
+                    {
+                        case "-0+":
+                            break;
+                        case "-+":
+                            if (vari == 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                        case "0+":
+                            if (vari < 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                        case "+":
+                            if (vari <= 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                        case "-0":
+                            if (vari > 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                        case "-":
+                            if (vari >= 0)
+                            {
+                                check = false;
+                            }
+                            break;
+                    }
+                }
+            }
+            while (!check);
+            return vari;
+        }
+        public static char Char(string text, string type, bool check)
+        {
+            char vari = ' ';
+            ConsoleKeyInfo e;
+            //Console.WriteLine();
+            do
+            {
+                if (!check)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Некорректные данные. Нажмите Enter");
+                }
+                try
+                {
+                    if (check == false)
+                    {
+                        while (true)
+                        {
+                            e = Console.ReadKey(true);
+                            if (e.Key == ConsoleKey.Escape)
+                            {
+                                MyInput.Exit();
+                            }
+                            else if (e.Key == ConsoleKey.Enter)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    Console.Write(text);
+                    vari = Convert.ToChar(Console.ReadLine());
+                    check = true;
+                }
+                catch
+                {
+                    check = false;
+                }
+            }
+            while (!check);
+            return vari;
+        }
+        public static string String(string text, string type, bool check)
+        {
+            string vari = "";
+            ConsoleKeyInfo e;
+            //Console.WriteLine();
+            do
+            {
+                if (!check)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Некорректные данные. Нажмите Enter");
+                }
+                try
+                {
+                    if (check == false)
+                    {
+                        while (true)
+                        {
+                            e = Console.ReadKey(true);
+                            if (e.Key == ConsoleKey.Escape)
+                            {
+                                MyInput.Exit();
+                            }
+                            else if (e.Key == ConsoleKey.Enter)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    Console.Write(text);
+                    vari = Convert.ToString(Console.ReadLine());
+                    check = true;
+                }
+                catch
+                {
+                    check = false;
+                }
+            }
+            while (!check);
+            return vari;
+        }
+    }
+    class Program
+    {
+
+        public static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Задача 14. Проверка дня недели для 2024 года");
+            Console.ResetColor();
+            int day = 0, month = 0, m;
+            string s;
+            ConsoleKeyInfo e;
+            Console.Write("Для запуска нажмите любую клавишу (Esc - прервать)");
+            e = Console.ReadKey(true);
+            if (e.Key == ConsoleKey.Escape)
+            {
+                MyInput.Exit();
+            }
+            int[] arrayOfDays = new int[12] {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+            Console.WriteLine();
+            bool check = true;
+            do
+            {
+                day = MyInput.Int32("Введите день: ", "+", check);
+                check = false;
+            }
+            while (day > 31);
+            check = true;
+            do
+            {
+                month = MyInput.Int32("Введите месяц: ", "+", check);
+                if (month > 12 || day > arrayOfDays[month - 1])
+                {
+                    check = false;
+                }
+                else check = true;
+            }
+            while (!check);
+            int dayOfWeek = 0;
+            for (int i = 0; i < month - 1; i++) 
+            {
+                for (int j = 0; j < arrayOfDays[i]; j++)
+                {
+                    dayOfWeek++;
+                    if (dayOfWeek == 7) dayOfWeek = 0;
+                }
+            }
+            for (int i = 0; i < day - 1; i++)
+            {
+                dayOfWeek++;
+                if (dayOfWeek == 7) dayOfWeek = 0;
+            }
+            Console.Write("{0}.{1}.2024 - это ", day, month);
+            switch (dayOfWeek)
+            {
+                case 0: Console.WriteLine("Понедельник"); break;
+                case 1: Console.WriteLine("Вторник"); break;
+                case 2: Console.WriteLine("Среда"); break;
+                case 3: Console.WriteLine("Четверг"); break;
+                case 4: Console.WriteLine("Пятница"); break;
+                case 5: Console.WriteLine("Суббота"); break;
+                case 6: Console.WriteLine("Воскресенье"); break;
+            }
+            MyInput.Exit();
+        }
+    }
+}
